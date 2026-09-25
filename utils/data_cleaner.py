da9658b382a18bf0c9b5318c3f27a5055ecce89c
Yassine Erradouani <yassine.erradouani@gmail.com>
2026-09-25 18:31:40 +0000

    Skip rows whose product name is blank

--- utils/data_cleaner.py
+++ utils/data_cleaner.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def clean_sales_data(df: pd.DataFrame) -> pd.DataFrame:
     df.dropna(subset=["date", "product", "quantity", "price"], inplace=True)
+    df = df.loc[df['product'].astype(str).str.strip().ne('')].copy()
     df['date'] = pd.to_datetime(df['date'], errors='coerce')
     df.dropna(subset=["date"], inplace=True)
     df['quantity'] = df['quantity'].astype(int)
