34629aad87eab40ecbffc7ebd335d766699af2c0
Yassine Erradouani <yassine.erradouani@gmail.com>
2026-09-25 18:31:40 +0000

    Remove sales rows with negative quantities

--- utils/data_cleaner.py
+++ utils/data_cleaner.py
@@ -5,5 +5,6 @@ def clean_sales_data(df: pd.DataFrame) -> pd.DataFrame:
     df['date'] = pd.to_datetime(df['date'], errors='coerce')
     df.dropna(subset=["date"], inplace=True)
     df['quantity'] = df['quantity'].astype(int)
+    df = df.loc[df['quantity'] >= 0].copy()
     df['price'] = df['price'].astype(float)
     return df
