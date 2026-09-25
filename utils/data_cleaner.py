5014b313e2b45018a72504edb20fa625cc809109
Yassine Erradouani <yassine.erradouani@gmail.com>
2026-09-25 18:31:41 +0000

    Store parsed sale dates without time components

--- utils/data_cleaner.py
+++ utils/data_cleaner.py
@@ -4,6 +4,7 @@ def clean_sales_data(df: pd.DataFrame) -> pd.DataFrame:
     df.dropna(subset=["date", "product", "quantity", "price"], inplace=True)
     df['date'] = pd.to_datetime(df['date'], errors='coerce')
     df.dropna(subset=["date"], inplace=True)
+    df['date'] = df['date'].dt.date
     df['quantity'] = df['quantity'].astype(int)
     df['price'] = df['price'].astype(float)
     return df
